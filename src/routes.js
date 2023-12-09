d07127c60aa204fa4c581d8102d8d35a5ba64f21
Wendel <luccawendel25@gmail.com>
2023-12-09 10:24:50 -0300

    novas rotas

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,18 @@ import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 import multer from 'multer';
 import uploadConfig from './config/upload';
+import DashboardController from './controllers/DashboardsController'
 
 const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post('/session', SessionController.store);
 
-routes.post('/houses', upload.single('thumbnail'), HouseController.store);
-
 routes.get('/houses', HouseController.index);
-
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
+routes.delete('/houses', HouseController.destroy);
+
+routes.get('/dashboard', DashboardController.show);
 
 export default routes;
